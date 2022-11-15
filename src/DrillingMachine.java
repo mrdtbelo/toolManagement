6b69970fe349ca3b9d14feaebaefd3c0f7658102
mrdt.07 <belomaridiat@gmail.com>
2022-11-15 18:59:35 +0100

    3

--- src/DrillingMachine.java
+++ src/DrillingMachine.java
@@ -1,5 +1,6 @@
 public class DrillingMachine {
 
+    // (210) Aufgabe 1
     private int watt;
     private String name;
     private int age;
@@ -8,62 +9,68 @@ public class DrillingMachine {
 
     public DrillingMachine() {
         this.name = "Driller";
-        this.watt = (int) (800 + (Math.random() * 1200));
-        this.age = (int) (Math.random() * 10);
-        this.price = ( 50+ (Math.random() * 500));
+        this.watt = (int) (Math.random() * 1201 + 800);
+        this.age = (int) (Math.random() * 11);
+        this.price = Math.random() * 450 + 50;
         this.broken = false;
     }
 
-    public int getWatt() {
-        return watt; }
-    public void setWatt(int watt) {
-        this.watt = watt;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
+    // (210) Aufgabe 2
+    public void becomeBroken() {
+        broken = true;
     }
 
-    public boolean isBroken() {
-        return broken;
+    public void repair() {
+        if (broken == true) {
+            broken = false;
+            price = price / 2;
+        }
     }
+
+    public void aging() {
+        age = age + 1;
+        if (age >= 10) {
+            becomeBroken();
+        }
+    }
+
+    public void reactOnHighDemand() {
+        price = price * 1.25;
+    }
+
+    // (210) Aufgabe 3
     public void setBroken(boolean broken) {
         this.broken = broken;
     }
 
-
-    public void becomeBroken() {
-        setBroken(true);
+    public void increasePrice(double amount) {
+        price = price + amount;
     }
 
-    public void repair() {
-        if (isBroken() == true){
-                setBroken(false);
-                setPrice(getPrice()/ 2);
+    public void changePower(int watt) {
+        if (watt > 0) {
+            this.watt = this.watt + watt;
+            if (watt > 100) {
+                this.price = price * 1.05;
+            }
+        } else if (watt < 0) {
+            this.watt = this.watt + watt;
+            this.price = price * 0.9;
         }
     }
-    public void aging() {
-        setAge(getAge() + 1 );
-        if (getAge() >= 10){
-            setBroken(true);
-        }
+
+    // (210) Aufgabe 4
+    public double getPrice() {
+        return price;
     }
 
-    public void reactOnHighDemand(){
-        setPrice(getPrice() + getPrice()*1.25);
+    public boolean isInexpensive() {
+        if (watt > 500 && price < 80) {
+            System.out.println("preiswert!!");
+        } else {
+            System.out.println("nicht  preiswert!!");
+        }
+        return false;
     }
 }
+
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -3,12 +3,9 @@ public class MainProgram {
     public static void main(String[] args) {
 
         DrillingMachine d1 = new DrillingMachine();
-        System.out.println(d1.getName() +"   "+ d1.getWatt() +"   "+ d1.getAge() +"   "+ d1.getPrice() +"   "+ d1.isBroken());
 
         DrillingMachine d2 = new DrillingMachine();
-        System.out.println(d2.getName() +"   "+ d2.getWatt() +"   "+ d2.getAge() +"   "+ d2.getPrice() +"   "+ d2.isBroken());
 
         DrillingMachine d3 = new DrillingMachine();
-        System.out.println(d3.getName() +"   "+ d3.getWatt() +"   "+ d3.getAge() +"   "+ d3.getPrice() +"   "+ d3.isBroken());
     }
 }
